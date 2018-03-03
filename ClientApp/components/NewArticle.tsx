e5c803ef133be0c90902d4fbddf25c50a9d13b2c
Bruzhmelev Anton <bruzhmelev@gmail.com>
2018-03-04 02:31:19 +0300

    Fixed html in admin panel

--- ClientApp/components/NewArticle.tsx
+++ ClientApp/components/NewArticle.tsx
@@ -11,7 +11,12 @@ import Input from "./Primiteves/Input";
 type NewArticleProps =
 typeof ArticlesState.actionCreators;      // ... plus action creators we've requested
 
-class NewArticle extends React.Component<NewArticleProps, {}> {
+interface NewArticleState {
+    title: string;
+    text: string;
+}
+
+class NewArticle extends React.Component<NewArticleProps, NewArticleState> {
 
     state = {
         title: "",
@@ -20,16 +25,22 @@ class NewArticle extends React.Component<NewArticleProps, {}> {
     
     public render() {
         return (
-            <div>
-                <h1>Новая статья</h1>
-                <label> Название:
-                    <input type='text' value={this.state.title} onChange={this.handleTitleChange}/>
-                </label>
-                <label> Текст:
-                    <textarea rows={40} cols={90} value={this.state.text} onChange={this.handleTextChange}/>
-                </label>
-                <button onClick={this.handleSave}>Save</button>
-            </div>
+            <form>
+                <h2>Новая статья</h2>
+                <div className="form-group">
+                    <label htmlFor="article-name">Название</label>
+                    <input id='article-name' className='form-control' type='text' value={this.state.title} onChange={this.handleTitleChange}/>
+                </div>
+                <div className="form-group">
+                    <label>Текст</label>
+                    <textarea className='form-control' rows={40} cols={90} value={this.state.text} onChange={this.handleTextChange}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={this.handleSave}>Save</button>
+            </form>
         );
     }
     
@@ -41,7 +52,7 @@ class NewArticle extends React.Component<NewArticleProps, {}> {
     }
 
     private handleTitleChange = (ev:any) =>{
-        //TODO: Как обернуть input в свой Input, чтобы инкапсулировать Валидацию, работу с ev, кастомизировать возвращаемые объекты на события onChange
+        //TODO: обернуть input в свой Input, чтобы инкапсулировать Валидацию, работу с ev, кастомизировать возвращаемые объекты на события onChange
         this.setState({title: ev.target.value});
     }
 
